0e41a3e495108ed9ceb6cbe03ae3e7b4531e3b22
Santiago Libonati <santiago.libonati@gmail.com>
2025-07-29 00:14:48 -0300

    Changed rgb to color and fixed PPM output

--- src/image_formats/ppm.rs
+++ src/image_formats/ppm.rs
@@ -1,40 +1,28 @@
+use crate::{color::Color, vec3::Precision};
 
-#[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
-pub struct RGB {
-    pub r: u8,
-    pub g: u8,
-    pub b: u8,
-}
-
-impl RGB {
-    pub fn new(r: u8, g: u8, b: u8) -> Self {
-        Self { r, g, b }
-    }
-}
-
-#[derive(Debug, PartialEq, Eq, Clone)]
+#[derive(Debug, PartialEq, PartialOrd, Clone)]
 pub struct PPM {
     cols: usize,
     rows: usize,
     max_color: u8,
-    values: Vec<RGB>,
+    values: Vec<Color>,
 }
 
 impl PPM {
-    pub fn new(cols: usize, rows: usize, max_color: u8, values: Vec<RGB>) -> Self {
+    pub fn new(cols: usize, rows: usize, max_color: u8, values: Vec<Color>) -> Self {
         assert!(values.len() == cols * rows);
         Self { cols, rows, max_color, values }
     }
 
     pub fn generate<F>(cols: usize, rows: usize, max_color: u8, gen: F) -> Self
     where 
-        F: Fn(f64, f64) -> RGB,
+        F: Fn(Precision, Precision) -> Color,
     {
         let mut values = Vec::with_capacity(cols * rows);
 
         for row in 0..rows {
             for col in 0..cols {
-                values.push(gen(row as f64, col as f64));
+                values.push(gen(row as Precision, col as Precision));
             }
         }
 
@@ -50,7 +38,7 @@ impl PPM {
             eprintln!("Scanlines remaining: {}", self.rows - row);
             for col in 0..self.cols {
                 let rgb = self.values[row * self.cols + col];
-                println!("{} {} {}", rgb.r, rgb.g, rgb.b);
+                println!("{} {} {}", (rgb.x() * 255.99) as i32, (rgb.y() * 255.99) as i32, (rgb.z() * 255.99) as i32);
             }
         }
 
@@ -72,14 +60,14 @@ mod tests {
             cols, rows, max_color,
             |row, col| {
 
-            let r = col / (cols-1) as f64;
-            let g = row / (rows-1) as f64;
-            let b = 0;
+            let r = col / (cols-1) as Precision;
+            let g = row / (rows-1) as Precision;
+            let b = 0.;
 
-            let r = (r * 255.0) as u8;
-            let g = (g * 255.0) as u8;
+            let r = r * 255.0;
+            let g = g * 255.0;
 
-            RGB::new(r, g, b)
+            Color::new(r, g, b)
         });
 
         ppm.output();
