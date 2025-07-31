66ab8f041de200d11cd1261c5e41cf8c416a2cff
Santiago Libonati <santiago.libonati@gmail.com>
2025-07-31 20:47:17 -0300

    Added 'fuzz' to metallic material

--- src/materials/metal.rs
+++ src/materials/metal.rs
@@ -1,24 +1,28 @@
-use crate::{figures::hittable::HitRecord, utility::{color::Color, ray::Ray}};
+use crate::{figures::hittable::HitRecord, utility::{color::Color, interval::Interval, ray::Ray, vec3::{Precision, Vec3}}};
 
 use super::material::{Material, ScatteredRay};
 
 #[derive(Debug)]
 pub struct Metal {
-    albedo: Color
+    albedo: Color,
+    fuzz: Precision,
 }
 
 impl Metal {
-    pub fn new(albedo: Color) -> Self {
-        Self { albedo }
+    pub fn new(albedo: Color, fuzz: Precision) -> Self {
+        let fuzz = if Interval::new(0.,1.).contains(fuzz) { fuzz } else { 1. };
+
+        Self { albedo, fuzz }
     }
 }
 
 impl Material for Metal {
     fn scatter(&self, ray: &Ray, rec: &HitRecord) -> Option<ScatteredRay> {
         let reflected = ray.direction().reflect(&rec.normal);
+        let reflected = reflected.unit_vec() + (self.fuzz * Vec3::random_unit_vec());
         let scattered = Ray::new(rec.p, reflected);
         let attenuation = self.albedo;
 
-        Some(ScatteredRay { ray: scattered, attenuation })
+        (scattered.direction().dot(&rec.normal) > 0.).then_some(ScatteredRay { ray: scattered, attenuation })
     }
 }
