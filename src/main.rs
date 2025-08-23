9979ce10844785de95ba3b5b52cfc0bdf68fe00d
Santiago Libonati <santiago.libonati@gmail.com>
2025-08-23 03:03:40 -0300

    Next Week final scene

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,15 @@ use std::{io::stdin, sync::Arc};
 
 use ray_tracing::{
     figures::{
-        bvh::BvhNode, camera::{Camera, DefocusSettings, ImageSettings, ViewSettings}, constant_medium::ConstantMedium, cube::Cube, hittable_list::HitList, quad::{QDisk, QRect, QTri}, rotate::Rotate, sphere::Sphere, translate::Translate
+        bvh::BvhNode,
+        camera::{Camera, DefocusSettings, ImageSettings, ViewSettings},
+        constant_medium::ConstantMedium,
+        cube::Cube,
+        hittable_list::HitList,
+        quad::{QDisk, QRect, QTri},
+        rotate::Rotate,
+        sphere::Sphere,
+        translate::Translate,
     },
     materials::{
         dielectric::Dielectric, emissive::DiffuseLight, lambertian::Lambertian, material::Material,
@@ -29,6 +37,8 @@ fn match_scene(scene: i32) {
         8 => simple_light(),
         9 => cornell_box(),
         10 => cornell_smoke(),
+        11 => next_week_final(800, 10000, 40),
+        -1 => next_week_final(400, 250, 4), // debug
         n => eprintln!("{n} is not a valid scene..."),
     }
 }
@@ -53,6 +63,7 @@ fn main() {
         eprintln!("8: Simple light");
         eprintln!("9: Cornell box");
         eprintln!("10: Cornell smoke");
+        eprintln!("11: Next week final render");
 
         eprintln!("Choose a scene: ");
         let stdin = stdin();
@@ -60,6 +71,134 @@ fn main() {
         assert!(stdin.read_line(&mut input).is_ok());
         match_scene(input.trim_end().parse().unwrap_or_default());
     }
+}
+
+fn next_week_final(image_width: i32, samples_per_pixel: i32, max_depth: i32) {
+    let mut boxes1 = HitList::new();
+    let ground = Arc::new(Lambertian::new(Color::new(0.48, 0.83, 0.53)));
+
+    let boxes_per_side = 20;
+    for i in 0..boxes_per_side {
+        for j in 0..boxes_per_side {
+            let w = 100.;
+            let x0 = -1000. + i as Precision * w;
+            let z0 = -1000. + j as Precision * w;
+            let y0 = 0.;
+            let x1 = x0 + w;
+            let y1 = random_f32(1., 101.);
+            let z1 = z0 + w;
+
+            boxes1.push(Arc::new(Cube::new(
+                Point3::new(x0, y0, z0),
+                Point3::new(x1, y1, z1),
+                ground.clone(),
+            )));
+        }
+    }
+
+    let mut world = HitList::new();
+
+    world.push(Arc::new(BvhNode::from_hitlist(boxes1)));
+
+    let light = Arc::new(DiffuseLight::from_color(Color::new(1., 1., 1.), 7.));
+    world.push(Arc::new(QRect::new(
+        Point3::new(123., 554., 147.),
+        Vec3::new(300., 0., 0.),
+        Vec3::new(0., 0., 265.),
+        light.clone(),
+    )));
+
+    let center1 = Point3::new(400., 400., 200.);
+    let center2 = center1 + Vec3::new(30., 0., 0.);
+    let sphere_material = Arc::new(Lambertian::new(Color::new(0.7, 0.3, 0.1)));
+    world.push(Arc::new(Sphere::new_animated(
+        center1,
+        center2,
+        50.,
+        sphere_material,
+    )));
+
+    let glass = Arc::new(Dielectric::new(1.5));
+    world.push(Arc::new(Sphere::new(
+        Point3::new(260., 150., 45.),
+        50.,
+        glass.clone(),
+    )));
+    world.push(Arc::new(Sphere::new(
+        Point3::new(0., 150., 145.),
+        50.,
+        Arc::new(Metal::new(Color::new(0.8, 0.8, 0.9), 1.)),
+    )));
+
+    let boundary = Arc::new(Sphere::new(
+        Point3::new(360., 150., 145.),
+        70.,
+        glass.clone(),
+    ));
+    world.push(boundary.clone());
+    world.push(Arc::new(ConstantMedium::from_color(
+        boundary,
+        0.2,
+        Color::new(0.2, 0.4, 0.9),
+    )));
+
+    let boundary = Arc::new(Sphere::new(Point3::new(0., 0., 0.), 5000., glass.clone()));
+    world.push(Arc::new(ConstantMedium::from_color(
+        boundary,
+        0.0001,
+        Color::new(1., 1., 1.),
+    )));
+
+    let emat = Arc::new(Lambertian::from_texture(Arc::new(ImageTexture::new(
+        "earthmap.jpg",
+    ))));
+    world.push(Arc::new(Sphere::new(
+        Point3::new(400., 200., 400.),
+        100.,
+        emat,
+    )));
+    let pertext = Arc::new(NoiseTexture::new(0.2));
+    world.push(Arc::new(Sphere::new(
+        Point3::new(220., 280., 300.),
+        80.,
+        Arc::new(Lambertian::from_texture(pertext)),
+    )));
+
+    let mut boxes2 = HitList::new();
+    let white = Arc::new(Lambertian::new(Color::new(0.73, 0.73, 0.73)));
+    let ns = 1000;
+    for _j in 0..ns {
+        boxes2.push(Arc::new(Sphere::new(
+            Point3::random_bounded(0., 165.),
+            10.,
+            white.clone(),
+        )));
+    }
+
+    world.push(Arc::new(Translate::new(
+        Arc::new(Rotate::new(
+            Arc::new(BvhNode::from_hitlist(boxes2)),
+            Vec3::new(0., 15., 0.),
+        )),
+        Vec3::new(-100., 270., 395.),
+    )));
+
+    let image_settings = ImageSettings {
+        aspect_ratio: 1.,
+        image_width,
+        samples_per_pixel,
+        max_depth,
+        background: Color::new(0., 0., 0.),
+    };
+    let view_settings = ViewSettings {
+        vfov: 40.,
+        look_from: Point3::new(478., 278., -600.),
+        look_at: Point3::new(278., 278., 0.),
+        vup: Vec3::new(0., 1., 0.),
+    };
+    let cam = Camera::new(image_settings, view_settings, Default::default());
+
+    cam.render(&world);
 }
 
 fn cornell_smoke() {
@@ -70,65 +209,69 @@ fn cornell_smoke() {
     let green = Arc::new(Lambertian::new(Color::new(0.12, 0.45, 0.15)));
     let light = Arc::new(DiffuseLight::from_color(Color::new(1., 1., 1.), 7.));
 
-    world.push(
-        Arc::new(QRect::new(
-            Point3::new(555., 0., 0.), 
-            Vec3::new(0.,555.,0.), 
-            Vec3::new(0.,0.,555.), 
-            green.clone(),
-        ))
-    );
-    world.push(
-        Arc::new(QRect::new(
-            Point3::new(0., 0., 0.), 
-            Vec3::new(0.,555.,0.), 
-            Vec3::new(0.,0.,555.), 
-            red.clone(),
-        ))
-    );
-    world.push(
-        Arc::new(QRect::new(
-            Point3::new(113., 554., 127.), 
-            Vec3::new(330., 0., 0.), 
-            Vec3::new(0.,0., 305.), 
-            light.clone(),
-        ))
-    );
-    world.push(
-        Arc::new(QRect::new(
-            Point3::new(0., 0., 0.), 
-            Vec3::new(555.,0.,0.), 
-            Vec3::new(0.,0.,555.), 
-            white.clone(),
-        ))
-    );
-    world.push(
-        Arc::new(QRect::new(
-            Point3::new(555., 555., 555.), 
-            Vec3::new(-555.,0.,0.), 
-            Vec3::new(0.,0.,-555.), 
-            white.clone(),
-        ))
-    );
-    world.push(
-        Arc::new(QRect::new(
-            Point3::new(0., 0., 555.), 
-            Vec3::new(555.,0.,0.), 
-            Vec3::new(0.,555.,0.), 
-            white.clone(),
-        ))
-    );
+    world.push(Arc::new(QRect::new(
+        Point3::new(555., 0., 0.),
+        Vec3::new(0., 555., 0.),
+        Vec3::new(0., 0., 555.),
+        green.clone(),
+    )));
+    world.push(Arc::new(QRect::new(
+        Point3::new(0., 0., 0.),
+        Vec3::new(0., 555., 0.),
+        Vec3::new(0., 0., 555.),
+        red.clone(),
+    )));
+    world.push(Arc::new(QRect::new(
+        Point3::new(113., 554., 127.),
+        Vec3::new(330., 0., 0.),
+        Vec3::new(0., 0., 305.),
+        light.clone(),
+    )));
+    world.push(Arc::new(QRect::new(
+        Point3::new(0., 0., 0.),
+        Vec3::new(555., 0., 0.),
+        Vec3::new(0., 0., 555.),
+        white.clone(),
+    )));
+    world.push(Arc::new(QRect::new(
+        Point3::new(555., 555., 555.),
+        Vec3::new(-555., 0., 0.),
+        Vec3::new(0., 0., -555.),
+        white.clone(),
+    )));
+    world.push(Arc::new(QRect::new(
+        Point3::new(0., 0., 555.),
+        Vec3::new(555., 0., 0.),
+        Vec3::new(0., 555., 0.),
+        white.clone(),
+    )));
 
-    let box1 = Arc::new(Cube::new(Point3::new(0., 0., 0.), Point3::new(165., 330., 165.), white.clone()));
+    let box1 = Arc::new(Cube::new(
+        Point3::new(0., 0., 0.),
+        Point3::new(165., 330., 165.),
+        white.clone(),
+    ));
     let box1 = Arc::new(Rotate::new(box1, Vec3::new(0., 15., 0.)));
     let box1 = Arc::new(Translate::new(box1, Vec3::new(265., 0., 295.)));
 
-    let box2 = Arc::new(Cube::new(Point3::new(0., 0., 0.), Point3::new(165., 165., 165.), white));
+    let box2 = Arc::new(Cube::new(
+        Point3::new(0., 0., 0.),
+        Point3::new(165., 165., 165.),
+        white,
+    ));
     let box2 = Arc::new(Rotate::new(box2, Vec3::new(0., -18., 0.)));
     let box2 = Arc::new(Translate::new(box2, Vec3::new(130., 0., 65.)));
 
-    world.push(Arc::new(ConstantMedium::from_color(box1, 0.01, Color::new(0., 0., 0.))));
-    world.push(Arc::new(ConstantMedium::from_color(box2, 0.01, Color::new(1., 1., 1.))));
+    world.push(Arc::new(ConstantMedium::from_color(
+        box1,
+        0.01,
+        Color::new(0., 0., 0.),
+    )));
+    world.push(Arc::new(ConstantMedium::from_color(
+        box2,
+        0.01,
+        Color::new(1., 1., 1.),
+    )));
 
     let image_settings = ImageSettings {
         aspect_ratio: 1.,
@@ -156,61 +299,57 @@ fn cornell_box() {
     let green = Arc::new(Lambertian::new(Color::new(0.12, 0.45, 0.15)));
     let light = Arc::new(DiffuseLight::from_color(Color::new(1., 1., 1.), 15.));
 
-    world.push(
-        Arc::new(QRect::new(
-            Point3::new(555., 0., 0.), 
-            Vec3::new(0.,555.,0.), 
-            Vec3::new(0.,0.,555.), 
-            green.clone(),
-        ))
-    );
-    world.push(
-        Arc::new(QRect::new(
-            Point3::new(0., 0., 0.), 
-            Vec3::new(0.,555.,0.), 
-            Vec3::new(0.,0.,555.), 
-            red.clone(),
-        ))
-    );
-    world.push(
-        Arc::new(QRect::new(
-            Point3::new(343., 554., 332.), 
-            Vec3::new(-130., 0., 0.), 
-            Vec3::new(0.,0., -105.), 
-            light.clone(),
-        ))
-    );
-    world.push(
-        Arc::new(QRect::new(
-            Point3::new(0., 0., 0.), 
-            Vec3::new(555.,0.,0.), 
-            Vec3::new(0.,0.,555.), 
-            white.clone(),
-        ))
-    );
-    world.push(
-        Arc::new(QRect::new(
-            Point3::new(555., 555., 555.), 
-            Vec3::new(-555.,0.,0.), 
-            Vec3::new(0.,0.,-555.), 
-            white.clone(),
-        ))
-    );
-    world.push(
-        Arc::new(QRect::new(
-            Point3::new(0., 0., 555.), 
-            Vec3::new(555.,0.,0.), 
-            Vec3::new(0.,555.,0.), 
-            white.clone(),
-        ))
-    );
+    world.push(Arc::new(QRect::new(
+        Point3::new(555., 0., 0.),
+        Vec3::new(0., 555., 0.),
+        Vec3::new(0., 0., 555.),
+        green.clone(),
+    )));
+    world.push(Arc::new(QRect::new(
+        Point3::new(0., 0., 0.),
+        Vec3::new(0., 555., 0.),
+        Vec3::new(0., 0., 555.),
+        red.clone(),
+    )));
+    world.push(Arc::new(QRect::new(
+        Point3::new(343., 554., 332.),
+        Vec3::new(-130., 0., 0.),
+        Vec3::new(0., 0., -105.),
+        light.clone(),
+    )));
+    world.push(Arc::new(QRect::new(
+        Point3::new(0., 0., 0.),
+        Vec3::new(555., 0., 0.),
+        Vec3::new(0., 0., 555.),
+        white.clone(),
+    )));
+    world.push(Arc::new(QRect::new(
+        Point3::new(555., 555., 555.),
+        Vec3::new(-555., 0., 0.),
+        Vec3::new(0., 0., -555.),
+        white.clone(),
+    )));
+    world.push(Arc::new(QRect::new(
+        Point3::new(0., 0., 555.),
+        Vec3::new(555., 0., 0.),
+        Vec3::new(0., 555., 0.),
+        white.clone(),
+    )));
 
-    let box1 = Arc::new(Cube::new(Point3::new(0., 0., 0.), Point3::new(165., 330., 165.), white.clone()));
+    let box1 = Arc::new(Cube::new(
+        Point3::new(0., 0., 0.),
+        Point3::new(165., 330., 165.),
+        white.clone(),
+    ));
     let box1 = Arc::new(Rotate::new(box1, Vec3::new(0., 15., 0.)));
     let box1 = Arc::new(Translate::new(box1, Vec3::new(265., 0., 295.)));
     world.push(box1);
 
-    let box2 = Arc::new(Cube::new(Point3::new(0., 0., 0.), Point3::new(165., 165., 165.), white));
+    let box2 = Arc::new(Cube::new(
+        Point3::new(0., 0., 0.),
+        Point3::new(165., 165., 165.),
+        white,
+    ));
     let box2 = Arc::new(Rotate::new(box2, Vec3::new(0., -18., 0.)));
     let box2 = Arc::new(Translate::new(box2, Vec3::new(130., 0., 65.)));
     world.push(box2);
